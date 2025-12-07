ebcb83127ac4a518376c7b084fd0ad29e969c801
Steve Nguyen <steve.nguyen@pz.com.au>
2025-12-08 09:48:47 +1000

    protect endpoints from harassment

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,13 @@ Route::get('/point-values', [PointValueController::class, 'index']);
 Route::get('/point-values/{pointValue}', [PointValueController::class, 'show']);
 
 // Game Routes
-Route::apiResource('games', GameController::class);
-Route::post('/games/join', [GameController::class, 'join']);
+Route::get('/games', [GameController::class, 'index']);
+Route::post('/games', [GameController::class, 'store'])->middleware('throttle:5,1'); // 5 game creations per minute
+Route::get('/games/{game}', [GameController::class, 'show']);
+Route::put('/games/{game}', [GameController::class, 'update']);
+Route::patch('/games/{game}', [GameController::class, 'update']);
+Route::delete('/games/{game}', [GameController::class, 'destroy']);
+Route::post('/games/join', [GameController::class, 'join'])->middleware('throttle:10,1'); // 10 joins per minute
 Route::patch('/games/{game}/status', [GameController::class, 'updateStatus']);
 
 // Player Routes (nested under games)
@@ -38,7 +43,7 @@ Route::prefix('games/{game}')->group(function () {
     Route::patch('/players/{player}', [PlayerController::class, 'update']);
     Route::delete('/players/{player}', [PlayerController::class, 'destroy']);
     Route::post('/players/{player}/activity', [PlayerController::class, 'updateActivity']);
-    
+
     // Vote Routes (nested under games)
     Route::get('/votes', [VoteController::class, 'index']);
     Route::post('/votes', [VoteController::class, 'store']);
